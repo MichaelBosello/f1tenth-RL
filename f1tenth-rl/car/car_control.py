d11511c0aa3289eddf14eeaf6cf51894b0918c86
Michael Bosello <michael.bosello@studio.unibo.it>
2020-05-24 14:35:25 +0200

    Add support to real f1tenth

--- f1tenth-rl/car/car_control.py
+++ f1tenth-rl/car/car_control.py
@@ -1,17 +1,20 @@
 import rospy
-import time
 from ackermann_msgs.msg import AckermannDriveStamped
+import time
 
-MAX_SPEED_REDUCTION = 2
-STEERING_SPEED_REDUCTION = 3
-LIGHTLY_STEERING_REDUCTION = 2
+MAX_SPEED_REDUCTION = 5
+STEERING_SPEED_REDUCTION = 5
+LIGHTLY_STEERING_REDUCTION = 2.4
 
 class Drive():
-  def __init__(self):
-    self.max_speed = rospy.get_param("max_speed", 2)
-    self.max_steering = rospy.get_param("max_steering", 0.4189)
+  def __init__(self, is_simulator=False):
+    topic = "/vesc/high_level/ackermann_cmd_mux/input/nav_0"
+    if is_simulator:
+      topic = "/drive"
+    self.max_speed = rospy.get_param("max_speed", 5)
+    self.max_steering = rospy.get_param("max_steering", 0.34)
     print("max_speed: ", self.max_speed, ", max_steering: ", self.max_steering)
-    self.drive_publisher = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=0)
+    self.drive_publisher = rospy.Publisher(topic, AckermannDriveStamped, queue_size=0)
 
   def forward(self):
     self.send_drive_command(self.max_speed/MAX_SPEED_REDUCTION, 0)
@@ -41,23 +44,31 @@ class Drive():
     self.drive_publisher.publish(ack_msg)
 
 if __name__ == '__main__':
-    rospy.init_node('drive_test')
-    drive = Drive()
-    while True:
-        print("Write command")
-        cmd = input()
-        if cmd == "w":
-            drive.forward()
-        if cmd == "s":
-            drive.backward()
-        if cmd == "a":
-            drive.lightly_left()
-        if cmd == "d":
-            drive.lightly_right()
-        if cmd == "aa":
-            drive.left()
-        if cmd == "dd":
-            drive.right()
-            
-        time.sleep(1)
-        drive.stop()
+  run_seconds = 0.6
+  rospy.init_node('drive_test')
+  drive = Drive()
+  while True:
+    print("Write command")
+    cmd = input()
+    start = time.time()
+    if cmd == "w":
+      while time.time() - start < run_seconds:
+        drive.forward()
+    if cmd == "s":
+      while time.time() - start < run_seconds:
+        drive.backward()
+    if cmd == "a":
+      while time.time() - start < run_seconds:
+        drive.lightly_left()
+    if cmd == "d":
+      while time.time() - start < run_seconds:
+        drive.lightly_right()
+    if cmd == "aa":
+      while time.time() - start < run_seconds:
+        drive.left()
+    if cmd == "dd":
+      while time.time() - start < run_seconds:
+        drive.right()
+    if cmd == "c":
+      exit()            
+
